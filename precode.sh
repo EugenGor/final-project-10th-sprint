90a3525db79c05e13e082fdbcc1115d8545721c2
EugEnGor <gen481@mail.ru>
2024-03-19 21:36:03 +0300

    second-commit

--- precode.sh
+++ precode.sh
@@ -6,13 +6,14 @@
 #   dir2
 #   dir3
 #       dir4
-mkdir -p /task/dir1 /task/dir2 /task/dir3/dir4
+
+mkdir -p $pwd/task/dir1 $pwd/task/dir2 $pwd/task/dir3/dir4
 
 # изменяем текущую директорию на task
-cd task
+cd $pwd/task
 
 # создаём пустой файл task/dir2/empty
-touch /task/dir2/empty
+touch $pwd/task/dir2/empty
 
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
@@ -26,7 +27,7 @@ chmod 764 /task/dir2/hello.sh
 ls "/task/dir2" > /task/dir2/list.txt
 
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-cp -r -T /task/dir2 /task/dir3/dir4
+cp -r /task/dir2 /task/dir3/dir4
 
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
@@ -36,10 +37,14 @@ find /task -name "*.txt" > /task/dir1/summary.txt
 echo "/task/dir2/list.txt" >> /task/dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
-NAME='Всем студентам'
+export NAME="Всем студентам"
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
+
+#к сожалению не могу проверить вывод, выдает ошибку /bin/sh: 2: ./task.sh: not found. 
+#Задал вопрос в пачку
+
 cat /task/dir2/hello.sh echo $NAME >> /task/dir1/summary.txt
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
@@ -50,7 +55,7 @@ cat /task/Практическое задание
 
 # ищем в файле "Практическое задание" строки, которые содержат слово "dir"
 # и затем отсортировываем их
-grep "dir" ./Практическое задание.txt | sort
+grep "dir" ./Практическое задание | sort
 
 # меняем текущую директорию на родительскую для task
 cd ..
